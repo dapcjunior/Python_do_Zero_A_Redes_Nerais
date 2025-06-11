1f57bbd1b82f10ec6b272c8f4b61aa323b891cb8
Daniel Pereira <danielpereiracamposjunior@gmail.com>
2025-06-10 23:01:38 -0300

    Inicando estudo
    Iniciando os estudos de python com a colecao 6 em 1

---
+++ helloWorld.py
@@ -0,0 +1,3 @@
+print("Hello, World Python") # pagina 55
+
+# prox pagina 60 
---
+++ testes0001.py
@@ -0,0 +1,13 @@
+# codigo pyhon
+
+print("Olá Amigo!")
+print("Tudo bem")
+
+# Codigos com vicios de linguagens
+
+print("Hello, World");
+print("Haw are you?")
+
+# codigo extraoado
+
+print("Bem vindo"); print("ao curso de Python ")
